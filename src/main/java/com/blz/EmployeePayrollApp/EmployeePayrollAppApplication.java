db21e27c9de3f5d375250acd7fa01f5668d51073
satya-patel <satyaprakashpatel1998@gmail.com>
2021-06-19 15:57:06 +0530

    employee spring app added

---
+++ src/main/java/com/blz/EmployeePayrollApp/EmployeePayrollAppApplication.java
@@ -0,0 +1,14 @@
+package com.blz.EmployeePayrollApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Payroll App");
+		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+	}
+
+}
