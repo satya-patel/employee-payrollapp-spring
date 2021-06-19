7e2d650e9044b3decb92c435a369f324f6ea06df
satya-patel <satyaprakashpatel1998@gmail.com>
2021-06-19 16:01:37 +0530

    demo of various http methods

--- src/main/java/com/blz/EmployeePayrollApp/EmployeePayrollAppApplication.java
+++ src/main/java/com/blz/EmployeePayrollApp/EmployeePayrollAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Payroll App");
+		
 		SpringApplication.run(EmployeePayrollAppApplication.class, args);
 	}
 
---
+++ src/main/java/com/blz/EmployeePayrollAppDTO/EPADTO.java
@@ -0,0 +1,19 @@
+package com.blz.EmployeePayrollAppDTO;
+
+public class EPADTO {
+	
+	public String name;
+	public long salary;
+
+	public EPADTO(String name, long salary) {
+		super();
+		this.name = name;
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "EmployeePayrollDTO [name=" + name + ", salary=" + salary + "]";
+	}
+
+}
