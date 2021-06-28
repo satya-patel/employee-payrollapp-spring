6a6d2276836b1915897ae5b4beaf1e6675b0a6c2
satya-patel <satyaprakashpatel1998@gmail.com>
2021-06-28 13:40:54 +0530

    lombok for logging used

--- src/main/java/com/blz/EmployeePayrollApp/EmployeePayrollAppApplication.java
+++ src/main/java/com/blz/EmployeePayrollApp/EmployeePayrollAppApplication.java
@@ -3,12 +3,14 @@ package com.blz.EmployeePayrollApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
-		
 		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		log.info("Employee Payroll App started");
 	}
-
 }
